f4f6e20d336db661abf9d3c8bbe50ca29bdf9aeb
agrusha <agrusha@unison.com>
2013-03-28 13:19:29 +0300

    copying of su-configure.h added to windows build

--- makeallvc9.bat
+++ makeallvc9.bat
@@ -1,6 +1,6 @@
 call "%VS90COMNTOOLS%\vsvars32.bat"
 
-set INSTALL_DIR = %2%
+set INSTALL_DIR=%2%
 
 cd win32
 @call autogen.cmd
@@ -13,6 +13,8 @@ if "%1%" == "release" (
     devenv.com libsofia_sip_ua_static.vcproj /build "Debug|Win32" || goto error
 	echo F | xcopy /E /Y /I Debug\libsofia_sip_ua_static.lib %INSTALL_DIR%\lib\libsofia_sip_ua.lib || goto error
 )
+cd ..\sofia-sip
+xcopy /E /Y /I su_configure.h %INSTALL_DIR%\include\sofia-sip\
 
 cd ..\..\libsofia-sip-ua
 FOR /F %f IN ('dir *.h /b/s') DO xcopy /E /Y /I %f %INSTALL_DIR%\include\sofia-sip\
