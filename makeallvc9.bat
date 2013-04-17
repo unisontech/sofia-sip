be6ee5e32b11b5d14a85699eb8d65a0f3b04f814
Dmitry Zhukov <dzhukov@unison.com>
2013-04-17 13:20:42 +0400

    Build using vc9

--- makeallvc9.bat
+++ makeallvc9.bat
@@ -1,3 +1,5 @@
+@echo on
+
 call "%VS90COMNTOOLS%\vsvars32.bat"
 
 set INITIAL_DIR=%cd%
@@ -8,16 +10,16 @@ cd win32
 cd libsofia-sip-ua-static
 
 if "%1%" == "release" (
-    devenv.com libsofia_sip_ua_static.vcproj /build "Release|Win32" || goto error
+    devenv.com libsofia_sip_ua_static_vc9.sln /build "Release|Win32" || goto error
 	echo F | xcopy /E /Y /I Release\libsofia_sip_ua_static.lib %INSTALL_DIR%\lib\libsofia_sip_ua.lib || goto error
 ) else (
-    devenv.com libsofia_sip_ua_static.vcproj /build "Debug|Win32" || goto error
+    devenv.com libsofia_sip_ua_static_vc9.sln /build "Debug|Win32" || goto error
 	echo F | xcopy /E /Y /I Debug\libsofia_sip_ua_static.lib %INSTALL_DIR%\lib\libsofia_sip_ua.lib || goto error
 )
-echo F | xcopy /E /Y /I ..\pthread\pthread.lib %INSTALL_DIR%\lib\pthread.lib || goto error
+echo F | xcopy /E /Y /I ..\pthreads-win32\pthread.lib %INSTALL_DIR%\lib\pthread.lib || goto error
 
 echo F | xcopy /E /Y /I ..\sofia-sip\su_configure.h %INSTALL_DIR%\include\sofia-sip\su_configure.h 
-cd ..\..\libsofia-sip-ua
+cd %INITIAL_DIR%\libsofia-sip-ua
 FOR /F %%f IN ('dir *.h /b/s') DO echo F | xcopy /E /Y /I %%f %INSTALL_DIR%\include\sofia-sip\
 
 cd %INITIAL_DIR%
